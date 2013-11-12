17ba8e82e73e11dc5146416d1e592f9ea2066c41
Paul <paul@E6530-NB5.uni-bremen.de>
2013-11-12 09:20:53 +0100

    Klasse Titel bearbeitet

--- src/de/akademie/logit/model/Titel.java
+++ src/de/akademie/logit/model/Titel.java
@@ -1,8 +1,31 @@
 package de.akademie.logit.model;
 
 import java.util.LinkedList;
+import java.util.List;
 
-public class Titel {
-
-	private LinkedList<String> titel;
+public class Titel
+{
+	private List<String> titel = new LinkedList<String>();
+	private final static int gold = 20;
+	private final static int landmin = 5; 
+	
+	public Titel()
+	{
+		this.titel.add( "Bauer" );
+		
+		
+		
+		
+		this.titel.add( "Koenig" );
+	}
+	
+	public int getGold()
+	{
+		return Titel.gold;
+	}
+	
+	public int getLandmin()
+	{
+		return Titel.landmin;
+	}
 }
