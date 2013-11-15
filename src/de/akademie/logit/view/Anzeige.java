ab15915c2f870db7f5757a9219f504bab5a3f41f
Paul <paul@E6530-NB5.uni-bremen.de>
2013-11-15 15:15:15 +0100

    View: Anzeige, Infobereich

--- src/de/akademie/logit/view/Anzeige.java
+++ src/de/akademie/logit/view/Anzeige.java
@@ -1,7 +1,15 @@
 package de.akademie.logit.view;
 
+import de.akademie.logit.model.Spieler;
+
+/**
+ * 
+ * @author paul
+ * 
+ */
 public class Anzeige
 {
+
 	private Anzeige()
 	{}
 
@@ -10,14 +18,13 @@ public class Anzeige
 		System.out.println( text );
 	}
 
-	public static void zeigeMenuAn( Menu menu )
+	public static void zeigeMenuAn( Spieler spieler, Menu menu )
 	{
+		System.out.println( "Spielerinfo" );
+		System.out.println( "===========" );
+		new Infobereich( spieler ).anzeigenInfo();
+		System.out.println();
 		menu.printText();
-	}
-
-	public static void zeigeInfobereich(Infobereich infobereich)
-	{
-
 	}
 
 }
--- src/de/akademie/logit/view/Infobereich.java
+++ src/de/akademie/logit/view/Infobereich.java
@@ -2,10 +2,13 @@ package de.akademie.logit.view;
 
 import de.akademie.logit.model.Spieler;
 
+/**
+ * 
+ * @author paul
+ * 
+ */
 public class Infobereich
 {
-
-	private String text;
 	private Spieler spieler;
 
 	public Infobereich( Spieler spieler )
@@ -15,7 +18,19 @@ public class Infobereich
 
 	public void anzeigenInfo()
 	{
-
+		System.out.println( "Name:                " + this.spieler.getName() );
+		System.out.println( "Titel:               " + this.spieler.getTitel() );
+		System.out.println( "Gold:                " + this.spieler.getGold() );
+		System.out.println( "Land mit Gebäuden:   " + this.spieler );
+		System.out.println( "Soldaten:            " + this.spieler.getSoldaten() );
+		System.out.println( "Steuersatz:          " + this.spieler.getSteuersatz() );
+		System.out.println( "Mehl:                " + this.spieler.getMehl() );
+		System.out.println( "Korn:                " + this.spieler.getKorn() );
+		System.out.println( "Dünger:              " + this.spieler.getDuenger() );
+		System.out.println( "Essensration:        " + this.spieler.getEssensration() );
+		System.out.println( "Bevölkerungsanzahl:  " + this.spieler.getBevoelkerungsanzahl() );
+		System.out.println( "Ich wurde sabotiert: " + ( this.spieler.isSabotageOpfer() == true ? "Ja" : "Nein" ) );
+		System.out.println( "Zufriedenheit:       " + this.spieler.getBevoelkerungszufriedenheit() );
 	}
 
 }
