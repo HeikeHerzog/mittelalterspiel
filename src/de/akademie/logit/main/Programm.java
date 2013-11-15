bcf16ab75125913cce4ffd11aa02b4e626a7352b
Paul <paul@E6530-NB5.uni-bremen.de>
2013-11-15 13:34:37 +0100

    scheissdrecksgit igitt igitt!!!!

--- src/de/akademie/logit/main/Programm.java
+++ src/de/akademie/logit/main/Programm.java
@@ -1,9 +1,24 @@
 package de.akademie.logit.main;
 
+import de.akademie.logit.view.Aktionsmenu;
+import de.akademie.logit.view.Anzeige;
+import de.akademie.logit.view.Gebaeudemenu;
+import de.akademie.logit.view.Handelswarenmenu;
+import de.akademie.logit.view.Landmenu;
+import de.akademie.logit.view.Sabotagemenu;
+import de.akademie.logit.view.Soldatenmenu;
+import de.akademie.logit.view.Spielerhauptmenumaske;
+
 public class Programm
 {
 	public static void main( String[] args )
 	{
-
+		Anzeige.zeigeMenuAn( Spielerhauptmenumaske.getInstance() );
+		Anzeige.zeigeMenuAn( Landmenu.getInstance() );
+		Anzeige.zeigeMenuAn( Gebaeudemenu.getInstance() );
+		Anzeige.zeigeMenuAn( Soldatenmenu.getInstance() );
+		Anzeige.zeigeMenuAn( Handelswarenmenu.getInstance() );
+		Anzeige.zeigeMenuAn( Sabotagemenu.getInstance() );
+		Anzeige.zeigeMenuAn( Aktionsmenu.getInstance() );
 	}
 }
